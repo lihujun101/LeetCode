9d38ab051eae391db1e577d2397defafaf6ed163
lihujun <lihujun101@163.com>
2018-09-26 21:46:36 +0800

    链表循环

--- LinkList/L141_linked-list-cycle.py
+++ LinkList/L141_linked-list-cycle.py
@@ -3,6 +3,24 @@ class ListNode(object):
         self.val = x
         self.next = None
 
+# 想不明白为什么超时了，O(n)时间复杂度
+# class Solution(object):
+#     def hasCycle(self, head):
+#         """
+#         :type head: ListNode
+#         :rtype: bool
+#         """
+#         boolval = False
+#         node = head
+#         while node is not None :
+#             node = node.next
+#             if node == head:
+#                 boolval = True
+#                 break
+#             elif node is None:
+#                 break
+#         return boolval
+
 
 class Solution(object):
     def hasCycle(self, head):
@@ -10,22 +28,27 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        boolval = False
-        node = head
-        while node is not None :
-            node = node.next
-            if node == head:
-                boolval = True
-                break
-            elif node is None:
-                break
-        return boolval
+        if head is None or head.next is None:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if slow is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next
+            # 这一步是为了fast.next.next是有效的
+            if fast.next is None:
+                return False
+            fast = fast.next
+        return True
 
 
 if __name__ == '__main__':
-    node1 = ListNode(1)
+    node1 = ListNode(3)
     node1.next = ListNode(2)
-    node1.next.next = ListNode(3)
+    node1.next.next = ListNode(0)
+    node1.next.next.next = ListNode(-4)
    # node1.next.next.next = node1
     print(1)
     s = Solution()
