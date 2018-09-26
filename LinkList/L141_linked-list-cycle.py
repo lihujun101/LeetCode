596afeca7815992ae1940f794d883f5bc2a44650
lihujun <lihujun101@163.com>
2018-09-26 20:53:13 +0800

    link-list

--- LinkList/L141_linked-list-cycle.py
+++ LinkList/L141_linked-list-cycle.py
@@ -10,10 +10,24 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        try:
-            node1 = head.next
-            node2 = head.next.next
-        except:
-            return False
-        pass
+        boolval = False
+        node = head
+        while node is not None :
+            node = node.next
+            if node == head:
+                boolval = True
+                break
+            elif node is None:
+                break
+        return boolval
 
+
+if __name__ == '__main__':
+    node1 = ListNode(1)
+    node1.next = ListNode(2)
+    node1.next.next = ListNode(3)
+   # node1.next.next.next = node1
+    print(1)
+    s = Solution()
+    s1 = s.hasCycle(node1)
+    print(s1)
